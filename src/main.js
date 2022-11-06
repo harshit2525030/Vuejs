8ffcfb4612e8ee836d62017dd1c472ebf23d5e91
harshit2525030 <harshit.singh@globallogic,com>
2022-11-06 10:00:02 +0530

    changes in main

--- src/main.js
+++ src/main.js
@@ -3,24 +3,9 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { ValidationProvider } from 'vee-validate';
 import router from './router'
-// import VueRouter from "vue-router";
-// import Login from "./components/Login";
-// import Register from "./components/Register";
 
-// For 'vee-validation'
 Vue.component('ValidationProvider', ValidationProvider);
 
-// // For 'vue-router'
-// Vue.use(VueRouter);
-// const routes = [
-//   { path: "/login", component: Login },
-//   { path: "/register", component: Register },
-// ];
-// const router = new VueRouter({
-//   routes,
-// });
-
-// Vue.config.productionTip = false
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
 new Vue({
