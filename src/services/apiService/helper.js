c9185e3fb43f5df5df42aac1c38347470cd07c06
harshit2525030 <harshit.singh@globallogic,com>
2022-11-07 14:01:28 +0530

    changes

--- src/services/apiService/helper.js
+++ src/services/apiService/helper.js
@@ -3,7 +3,11 @@ import Axios from 'axios';
 export function createAxiosInstance() {
     const axios = Axios.create();
 
-    axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+    // axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+    // Below code works when env is no production...
+    if (process.env.NODE_ENV === "production") {
+        axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+    }
     axios.defaults.headers.common['Content-Type'] = 'application/json';
     // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('Token')}`;
     axios.defaults.timeout = process.env.VUE_APP_API_TIMEOUT;
